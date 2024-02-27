1db0fc579516e04a2ceeb96ce7b6d2b9a3c55924
MartinMedina1 <medinanmartin@javeriana.edu.co>
2024-02-27 16:35:25 -0500

    intento de subir css al servidor

---
+++ src/main/java/com/wiki/wiki/WebConfig.java
@@ -0,0 +1,16 @@
+package com.wiki.wiki;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+    }
+}
