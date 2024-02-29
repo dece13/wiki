91ccd5d8e72dab34c76a22c3395f3a6736257871
dece29 <castedaniel29@gmail.com>
2024-02-28 20:46:01 -0500

    elimine un resource que no servia y actualice webCOnfig

--- src/main/java/com/wiki/wiki/WebConfig.java
+++ src/main/java/com/wiki/wiki/WebConfig.java
@@ -4,11 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/css/**", "/js/**", "/images/**")
+                .addResourceLocations("classpath:/static/css/", "classpath:/static/js/", "classpath:/static/images/");
     }
 }
