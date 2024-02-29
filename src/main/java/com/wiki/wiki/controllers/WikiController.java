678ace1652e2ab3eb653cf07c27d1032b3340fa0
dece29 <castedaniel29@gmail.com>
2024-02-28 20:51:07 -0500

    Quite el index a ver

--- src/main/java/com/wiki/wiki/controllers/WikiController.java
+++ src/main/java/com/wiki/wiki/controllers/WikiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WikiController {
 
-    @GetMapping("/grupo13/home")
+    @GetMapping("/")
     public String home(){
          return "index.html";
      }
